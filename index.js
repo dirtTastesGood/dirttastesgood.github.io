bb23a3c16a2a1b0eb93aca3d2bbc88d2be6aeead
dirtTastesGood <keegood8@gmail.com>
2020-12-15 16:34:58 -0800

    revamp bio section

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-window.addEventListener("load", function(){
-    let container = document.getElementById("mandala-container");
-    container.style.opacity = 1;
-    for(i=1; i <= 8; i++){
-        let layer = document.getElementById("layer"+i);
-            layer.style.opacity = 1;
-    }
+window.addEventListener('load', function () {
+  let container = document.getElementById('mandala-container');
+  container.style.opacity = 1;
+  for (i = 1; i <= 8; i++) {
+    let layer = document.getElementById('layer' + i);
+    layer.style.opacity = 1;
+  }
 
-    let initials = document.getElementById("initials")
-    initials.style.opacity = 1;
+  let initials = document.getElementById('initials');
+  initials.style.opacity = 1;
 
-    downArrowContainer = document.getElementById("down-arrow-container-1");
-    downArrowContainer.style.opacity = 1;
-    
-    downArrow_1 = document.getElementById("down-arrow-1");
-    downArrow_1.style.opacity = 1;
-
-    mandalaBorder = document.getElementById("mandala-border");
-    mandalaBorder.style.opacity = 1;
-
+  let splash = document.querySelector('#splash');
+  console.log('WTF');
+  splash.classList.add('glow');
 });
